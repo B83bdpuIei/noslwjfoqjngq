afe066659eb5da85e1c23f991640ba7a5993f916
B83bdpuIei <142633035+B83bdpuIei@users.noreply.github.com>
2026-05-06 14:04:09 +0200

    Update keep_alive.py

--- mi-bot-qo/keep_alive.py
+++ mi-bot-qo/keep_alive.py
@@ -1,4 +1,3 @@
-# keep_alive.py
 from flask import Flask
 from threading import Thread
 
@@ -6,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "¡El bot de Mejora de Vida está online y funcionando!"
+    return "Bot is alive and running!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
