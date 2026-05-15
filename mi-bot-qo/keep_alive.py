5d773b83576b95bab0510d3773f2a8b885e65ff4
B83bdpuIei <142633035+B83bdpuIei@users.noreply.github.com>
2026-05-15 04:02:53 +0200

    Update keep_alive.py

--- mi-bot-qo/keep_alive.py
+++ mi-bot-qo/keep_alive.py
@@ -5,13 +5,13 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive and running!"
+    return "Cromi está despierta y activa."
 
 def run():
+    # Aquí es donde va el puerto, pero dentro de su función para que no se dispare solo
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
+    # Esto crea un "hilo" secundario para que la web y el bot funcionen a la vez sin chocarse
     t = Thread(target=run)
     t.start()
-# Tiene que ser 0.0.0.0 para que Render lo detecte, no dejes que el host esté vacío.
-    app.run(host='0.0.0.0', port=8080)
