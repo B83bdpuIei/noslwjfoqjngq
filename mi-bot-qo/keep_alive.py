d234e7f3428e02228858a6ad348e0b5b69e57b53
B83bdpuIei <142633035+B83bdpuIei@users.noreply.github.com>
2026-05-15 02:14:19 +0200

    Update keep_alive.py

--- mi-bot-qo/keep_alive.py
+++ mi-bot-qo/keep_alive.py
@@ -13,3 +13,5 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
+# Tiene que ser 0.0.0.0 para que Render lo detecte, no dejes que el host esté vacío.
+    app.run(host='0.0.0.0', port=8080)
